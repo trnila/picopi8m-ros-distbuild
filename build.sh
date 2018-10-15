c9f577b90574f4e975027b2506131ff34acf4f64
Daniel Trnka <daniel.trnka@gmail.com>
2018-10-15 18:14:54 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -x
+set -e
+
+DEST=./rootfs
+. ./config.sh
+
+DEST=$(realpath "$DEST")
+
+if [ -d "$DEST" ]; then
+  echo "Directory $DEST already exists, refusing to build"
+  exit 1
+fi
+
+
+# build kernel
+if [ ! -d linux ]; then
+  git clone --depth 1 https://github.com/trnila/linux-tn linux
+  git checkout "$KERNEL_BRANCH"
+fi
+(
+  cd linux
+  export CROSS_COMPILE=aarch64-linux-gnu-
+  export ARCH=arm64
+  make tn_imx8_defconfig
+  make "-j$(nproc)"
+
+  mkdir -p "$DEST/boot"
+  cp -v arch/arm64/boot/Image "$DEST/boot"
+  cp -v arch/arm64/boot/dts/freescale/*.dtb "$DEST/boot"
+  make modules_install INSTALL_MOD_PATH="$DEST/"
+)
+
+
+cleanup() {
+  umount "$DEST/mnt"
+}
+trap cleanup EXIT
+
+debootstrap --foreign --arch arm64 "$DISTRO" "$DEST"  http://ports.ubuntu.com/
+mkdir -p "$DEST/mnt"
+mount --bind ./work "$DEST/mnt"
+chroot "$DEST" /bin/sh /mnt/setup.sh
+
+echo "Successfully built to $DEST"
---
+++ config.sh
@@ -0,0 +1,15 @@
+export DISTRO=bionic
+export HOSTNAME=picopi
+export PACKAGES="
+  openssh-server
+  vim
+  htop
+  tmux
+  strace
+  ranger
+  python3-vcstool
+  git
+  gcc-arm-none-eabi
+  minicom
+  device-tree-compiler
+"
