a60305019bfdfbb1e73952636e0d2ec25d202f2b
Daniel Trnka <daniel.trnka@gmail.com>
2018-10-15 19:16:12 +0200

    fixed some issues in setup.sh

--- work/setup.sh
+++ work/setup.sh
@@ -6,20 +6,20 @@ export PATH=/sbin:/usr/sbin:/bin/:/usr/bin
 /debootstrap/debootstrap --second-stage
 
 # copy base files
-cp -r /mnt/files/ /
+cp -r /mnt/files/* /
 
 # setup hostname
-systemctl set-hostname "$HOSTNAME"
+echo "$HOSTNAME" > /etc/hostname
 echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
 echo "::1 $HOSTNAME" >> /etc/hosts
 
-apt-get install gnupg
+apt-get install -y gnupg
 
 # install ros
 mkdir -p /etc/apt/sources.list.d/
-echo "deb http://packages.ros.org/ros/ubuntu $DISTRO main" > /etc/apt/sources.list.d/ros.list
+echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu $DISTRO main" > /etc/apt/sources.list.d/ros.list
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 apt-get update
-apt-get install ros-melodic-ros-base
+apt-get install -y ros-melodic-ros-base
 
-apt install "$PACKAGES" 
+apt-get install -y "$PACKAGES" 
