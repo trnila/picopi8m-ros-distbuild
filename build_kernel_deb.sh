ee04ca884d3ebf28313cf24db073bbf74010659f
Daniel Trnka <daniel.trnka@gmail.com>
2018-12-03 00:08:27 +0100

    build_kernel_deb: refactored, fixed building linux-headers and added linux-libc-dev for userspace api

--- build_kernel_deb.sh
+++ build_kernel_deb.sh
@@ -2,66 +2,123 @@
 set -e
 set -x
 
+DEBUG=0
+if [ "$1" == "-d" ]; then
+  DEBUG=1
+fi
+
 . ./common.sh
 
-./build_kernel.sh
+if [ $DEBUG -eq 0 ]; then
+  ./build_kernel.sh
+fi
 
-# tempory directory for package build
+# tempory directory for packages build
 TEMP_DIR=$(mktemp -d)
 trap 'rm -r "$TEMP_DIR"' EXIT
 
-cd linux
+build() {
+  name="$1"
 
+  package_name="${name%*-dev}-picopim8"
+  package_name="${package_name//_/-}"
+
+  if [[ "$name" == *-dev ]]; then
+    package_name="${package_name}-dev"
+  fi
+
+  dst="$TEMP_DIR/$package_name"
+
+  mkdir -p "$dst/DEBIAN/"
+  cat > "$dst/DEBIAN/control" << EOF
+Package: $package_name
+Version: ${version:1}
+Architecture: arm64
+Maintainer: Daniel Trnka
+Section: kernel
+EOF
+
+"build_$name" "$dst"
+dpkg-deb -b "$dst"
+}
+
+build_linux_image() {
+  dst="$1"
+
+  cat >> "$dst/DEBIAN/control" << EOF
+Provides: linux-image, linux-modules
+Description: Modified linux kernel for pico-pi-imx8m
+ This package contains the Linux kernel, modules and device tree blobs
+EOF
+
+install -Dv ./arch/arm64/boot/Image "$dst/boot/Image"
+install -Dv ./arch/arm64/boot/dts/freescale/pico*.dtb "$dst/boot/"
+make modules_install INSTALL_MOD_PATH="$dst"
+rm "$dst/lib/modules/4.9.88${version}/build" # remove broken symlink
+rm "$dst/lib/modules/4.9.88${version}/source" # remove broken symlink
+}
+
+build_linux_headers() {
+  dst="$1"
+
+  cat >> "$dst/DEBIAN/control" << EOF
+Provides: linux-headers
+Description: Modified linux kernel-headers used for building out-of-tree kernel modules for pico-pi-imx8m
+EOF
+
+  # populate aarch64-linux-gnu-gcc compiler as gcc and add to the PATH
+  # so we can build scripts for arm target
+  mkdir "$dst/fake_bin"
+  for file in $(dirname "$(which aarch64-linux-gnu-gcc)")/aarch64-linux-gnu-*; do
+    name=$(basename "$file")
+    ln -s $file "$dst/fake_bin/${name#aarch64-linux-gnu-}"
+  done
+  export PATH="$dst/fake_bin:$PATH"
+
+  make M=scripts clean
+  export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu/
+  make scripts
+
+  MODULES="$dst/lib/modules/4.9.88${version}/build"
+  mkdir -p "$MODULES"
+  (
+  find . -name 'Kconfig*' -o -name 'Makefile*' -o -name '*.pl';
+  find arch/{arm,arm64}/include include scripts -type f 
+  find arch/arm64 -name module.lds -o -name Kbuild.platforms -o -name Platform
+  find $(find arch/arm64 -name include -o -name scripts -type d) -type f
+  find arch/arm64/include Module.symvers include scripts -type f
+  find .config
+  ) | tar -c -T - -f - | (cd "$MODULES"; tar -xf -)
+
+  make M=scripts clean
+}
+
+build_linux_libc_dev() {
+  dst="$1"
+
+  cat >> "$dst/DEBIAN/control" << EOF
+Provides: linux-libc-dev
+Description: Linux support headers for userspace development
+ This package provides userspaces headers from the Linux kernel.  These
+ headers are used by the installed headers for GNU libc and other system
+ libraries.
+EOF
+
+make headers_install ARCH=arm64 INSTALL_HDR_PATH="$dst/usr"
+}
+
+cd linux
 version=$(./scripts/setlocalversion)
 
-# prepare package
-mkdir -p "$TEMP_DIR/linux/DEBIAN"
-cat > "$TEMP_DIR/linux/DEBIAN/control" << EOF
-Package: linux
-Version: 1 
-Architecture: arm64
-Maintainer: Daniel Trnka
-Provides: linux-image, linux-modules
-Section: kernel
-Description: Modified linux kernel for pico-pi-imx8m
- This package contains the Linux kernel, modules and corresponding other files
-EOF
+build linux_image
+build linux_headers
+build linux_libc_dev
 
-mkdir -p "$TEMP_DIR/linux-headers/DEBIAN"
-cat > "$TEMP_DIR/linux-headers/DEBIAN/control" << EOF
-Package: linux-headers
-Version: 1 
-Architecture: arm64
-Maintainer: Daniel Trnka
-Provides: linux-headers
-Section: kernel
-Description: Modified linux kernel-headers for pico-pi-imx8m
-EOF
+du -sh "$TEMP_DIR"/*.deb 
+cp "$TEMP_DIR/"*.deb ../work/packages/
 
-# copy kernel, dtb's and modules
-mkdir -p "$TEMP_DIR/linux/boot/"
-cp -v "./arch/arm64/boot/Image" "$TEMP_DIR/linux/boot/"
-cp -v "./"/arch/arm64/boot/dts/freescale/pico*.dtb "$TEMP_DIR/linux/boot/"
-make modules_install INSTALL_MOD_PATH="$TEMP_DIR/linux/"
-
-# copy linux-headers
-MODULES="$TEMP_DIR/linux-headers/lib/modules/4.9.88${version}/build"
-rm "$TEMP_DIR/linux/lib/modules/4.9.88${version}/build" # remove broken symlink from linux package
-mkdir -p "$MODULES"
-cp -rv {.config,Makefile,scripts,include} "$MODULES"
-find . -name 'Kconfig*' -exec install -D {} "$MODULES/{}" \;
-find . -name 'Makefile*' -exec install -D {} "$MODULES/{}" \;
-find . -name '*.h' -exec install -D {} "$MODULES/{}" \;
-# remove unneeded files
-rm -rf "$MODULES"/drivers/gpu/drm/{amd,radeo,nouveau}
-find "$MODULES" -path '*nouveau*' -delete
-for arch in "$MODULES"/arch/*; do
-  [[ ! $arch = */arm64/ ]] && rm -r "$arch"
-done
-find "$MODULES" -type f -name '*.o' -delete
-
-# build packages
-dpkg-deb -b "$TEMP_DIR/linux/"
-dpkg-deb -b "$TEMP_DIR/linux-headers/"
-mv "$TEMP_DIR"/*.deb ../work/packages/
-echo "Packages built successfully"
+if [ "$DEBUG" -eq 1 ]; then
+  echo Entering build directory.....
+  cd "$TEMP_DIR"
+  bash
+fi
