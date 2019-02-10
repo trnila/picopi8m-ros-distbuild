91f5300f344c667b71d4fd28b92d14ac621e9a27
Daniel Trnka <daniel.trnka@gmail.com>
2019-02-10 15:01:38 +0100

    changed branch name to tn

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 export DEST=./rootfs
 export DISTRO=bionic
 export HOSTNAME=picopi
-export KERNEL_BRANCH=imx_4.9.88_2.0.0_ga-test-remoteproc
+export KERNEL_BRANCH=tn_4.9.88_2.0.0_ga-test-remoteproc
 export PACKAGES="
   ros-melodic-ros-base
   openssh-server
